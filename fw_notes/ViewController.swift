0391ec66be45c8a72763f304118f6665c71824ff
alex2024fw <alex2024fwedu@gmail.com>
2025-05-30 11:14:00 +0800

    Initial project setup for fw_notes app

---
+++ fw_notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fw_notes
+//
+//  Created by Fung Wing on 30/5/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fw_notesTests/fw_notesTests.swift
@@ -0,0 +1,17 @@
+//
+//  fw_notesTests.swift
+//  fw_notesTests
+//
+//  Created by Fung Wing on 30/5/2025.
+//
+
+import Testing
+@testable import fw_notes
+
+struct fw_notesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
